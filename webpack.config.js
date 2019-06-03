d92597171cc7ca617723bfc5220274632aba5905
Dragoș Străinu <str.dr4605@gmail.com>
2019-06-03 22:32:07 +0300

    configure images import for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const outputDir = path.join(__dirname, 'build/');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const outputDir = path.join(__dirname, "build/");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"]
       },
-    ],
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              limit: 10000,
+              name: "static/images/[name].[ext]"
+            }
+          },
+
+          {
+            loader: "img-loader",
+            options: {
+              enabled: true,
+              optipng: true
+            }
+          }
+        ]
+      }
+    ]
   },
-  entry: './lib/js/src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
+  entry: "./lib/js/src/Index.bs.js",
+  mode: isProd ? "production" : "development",
   output: {
     path: outputDir,
-    filename: 'Index.js'
+    filename: "Index.js"
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
       inject: true
     })
   ],
